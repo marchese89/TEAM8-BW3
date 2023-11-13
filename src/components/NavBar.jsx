7ef3c01de9fc76a681f8111f8e6355be2b15c1c0
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-13 19:55:38 +0100

    First Commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,63 @@
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Logo from "./src/LogoLinkedin";
-import { Form } from "react-router-dom";
-import { Button, FormControl } from "react-bootstrap";
-import { Icon } from "bootrap-icon";
+import { FormControl } from "react-bootstrap";
+import img from "../img/LogoLinkedin.jpg";
+import {
+  HouseDoorFill,
+  PeopleFill,
+  ChatRightDotsFill,
+  BellFill,
+} from "react-bootstrap-icons";
 
 function NavBar() {
+  const FormConrolStyle = {
+    maxWidth: "250px",
+  };
+  const iconTextStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginLeft: "10px",
+  };
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img src={Logo} alt="Logo Linkedin" width="30" height="30" />
-        </Navbar.Brand>
+      <Navbar.Brand href="#home">
+        <img src={img} alt="Logo Linkedin" width="30" height="30" />
+      </Navbar.Brand>
 
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Cerca"
-            className="mr-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Cerca</Button>
-        </Form>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#">
-                <i class="bi bi-house-door-fill"></i> Home
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#">
-                <i class="bi bi-people-fill"></i>Rete
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#">
-                <i class="bi bi-suitcase-lg"></i>Lavoro
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#">
-                <i class="bi bi-chat-right-dots-fill"></i>Messagistica
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#">
-                <i class="bi bi-bell-fill"></i>Notifiche
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <FormControl
+        type="search"
+        placeholder="Cerca"
+        className="serch"
+        aria-label="Search"
+        style={FormConrolStyle}
+      />
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <div style={iconTextStyle}>
+            <HouseDoorFill />
+            <span>Home</span>
+          </div>
+          <div style={iconTextStyle}>
+            <PeopleFill />
+            <span>Rete</span>
+          </div>
+          <div style={iconTextStyle}>
+            <i class="fas fa-suitcase"></i>
+            <span>Lavoro</span>
+          </div>
+          <div style={iconTextStyle}>
+            <ChatRightDotsFill />
+            <span> Messagistica</span>
+          </div>
+          <div style={iconTextStyle}>
+            <BellFill />
+            <span>Notifiche</span>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
