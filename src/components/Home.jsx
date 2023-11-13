41872afd7e27200dd51737d27182fbf01e54d38a
MaurizioCr <crispinomaurizio034@gmail.com>
2023-11-13 16:56:20 +0100

    Responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, NavLink, Row } from 'react-bootstrap';
 import styled from 'styled-components';
 import { HandThumbsUp, ChatText, Share, SendFill } from 'react-bootstrap-icons';
 const StyledButton = styled.background
@@ -53,17 +53,17 @@ const Home = () => {
       {postData.map(post => (
         < Row className='justify-content-center grow-0' key={post._id}>
           <Col className=' d-sm border mb-5 mt-5 bg-white rounded col-md-6  pt-2' >
-            <p style={{ fontSize: 16 + 'px', fontWeight: 'bold', margin: 0.2 + 'em' }}>Username: {post.username}</p>
-            <p style={{ fontSize: 10 + 'px' }}>Creazione: {formatData(post.createdAt)}</p>
-            <p style={{ fontSize: 16 + 'px' }}>Contenuto del post: {post.text}</p>
-            <p style={{ fontSize: 10 + 'px', margin: 0.2 + 'em' }}>Aggiornamento: {formatData(post.updatedAt)}</p>
-            <p style={{ fontSize: 10 + 'px' }}>ID: {post._id}</p>
+            <p style={{ fontSize: 1 + 'em', fontWeight: 'bold', margin: 0.2 + 'em' }}>Username: {post.username}</p>
+            <p style={{ fontSize: 0.7 + 'em' }}>Creazione: {formatData(post.createdAt)}</p>
+            <p style={{ fontSize: 1 + 'em' }}>Contenuto del post: {post.text}</p>
+            <p style={{ fontSize: 0.7 + 'em', margin: 0.2 + 'em' }}>Aggiornamento: {formatData(post.updatedAt)}</p>
+            <p style={{ fontSize: 0.7 + 'em' }}>ID: {post._id}</p>
             <hr />
-            <div className='d-flex'  >
-              <p className='align-items-start align-text-center me-3'><HandThumbsUp className='align-center me-1' />Consiglia</p>
-              <p className='align-items-start align-text-center me-3'><ChatText className='align-center me-1' />Commenta</p>
-              <p className='align-items-start align-text-center me-3'><Share className='align-center me-1' />Diffondi il post</p>
-              <p className='align-items-start align-text-center'><SendFill className='align-center me-1' />Invia</p>
+            <div className='d-flex flex-wrap'  >
+              <p className='align-items-start align-text-center me-3'><HandThumbsUp className='align-center me-1' />Consiglia</p> {/* Stato per mettere like */}
+              <p className='align-items-start align-text-center me-3'><ChatText className='align-center me-1' />Commenta</p> {/* Stato per aprire sezione commenti  */}
+              <NavLink className='align-items-start align-text-center me-3'><Share className='align-center me-1' />Diffondi il post</NavLink>
+              <NavLink className='align-items-start align-text-center'><SendFill className='align-center me-1' />Invia</NavLink>
             </div>
           </Col >
         </Row >
