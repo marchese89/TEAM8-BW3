a11d0f92b0861cf516488f489efec06626e8e216
marchese89 <marchese89@hotmail.com>
2023-11-13 12:32:09 +0100

    .

--- src/redux/reducers/profile-reducer.js
+++ src/redux/reducers/profile-reducer.js
@@ -2,4 +2,9 @@ const initialState = {
   profile: {},
 };
 
-export default function profileReducer(state = initialState, action) {}
+export default function profileReducer(state = initialState, action) {
+  switch (action) {
+    default:
+      return state;
+  }
+}
