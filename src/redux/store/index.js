d630afb212023af9bb503cbba5a269b625adf2a3
marchese89 <marchese89@hotmail.com>
2023-11-13 10:58:20 +0100

    start

---
+++ src/redux/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import songsSearchReducer from "../reducers/songs_search";
+// import songReducer from "../reducers/song";
+
+const bigReducer = combineReducers({
+  //   search: songsSearchReducer,
+  //   song: songReducer,
+});
+
+const store = configureStore({
+  reducer: bigReducer,
+});
+
+export default store;
