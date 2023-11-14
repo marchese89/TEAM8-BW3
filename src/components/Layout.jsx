53d627c3300bc7d24ff5447694a56e8e5cb258ab
marchese89 <marchese89@hotmail.com>
2023-11-14 18:48:41 +0100

    ...

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,12 @@ import { Outlet } from "react-router-dom";
 // import Profile from "./Profile";
 // import Experience from "./Experience";
 import ExperienceToModify from "./ExperienceToModify";
-import ExperienceModal from "./ExperienceModal";
+// import ExperienceModal from "./ExperienceModal";
 
 const Layout = () => {
   return (
     <>
       <ExperienceToModify />
-      <ExperienceModal />
       <Outlet />
     </>
   );
