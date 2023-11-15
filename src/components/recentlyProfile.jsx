ec8c3726b7bd6c07ffdcb8d4a935c4d1c5e3558c
federicopoggi <138136326+federicopoggi@users.noreply.github.com>
2023-11-15 23:11:59 +0100

    change

--- src/components/recentlyProfile.jsx
+++ src/components/recentlyProfile.jsx
@@ -1,3 +1,16 @@
-const RecentProfile = () => {};
+import { useEffect } from "react";
+import { Card } from "react-bootstrap";
+
+import styled from "styled-components";
+
+const StyledUse = styled.div``;
+
+const RecentProfile = () => {
+  return (
+    <StyledUse>
+      <Card></Card>
+    </StyledUse>
+  );
+};
 
 export default RecentProfile;
