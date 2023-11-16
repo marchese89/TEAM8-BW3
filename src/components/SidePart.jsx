5b6ea943dc2d7c92040c36b874ab1a84bf68944a
Federico Poggi <federicopoggi3@gmail.com>
2023-11-16 12:22:41 +0100

    333

--- src/components/SidePart.jsx
+++ src/components/SidePart.jsx
@@ -17,8 +17,8 @@ const StyledSide = styled.div`
 `;
 
 const SidePart = () => {
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUyMTY3N2M1NWU3ZTAwMThmODNjMmUiLCJpYXQiOjE2OTk4Nzg1MjEsImV4cCI6MTcwMTA4ODEyMX0.OUjIWiA8Coq8XIN7HBfmcox3tAoSACAyfSlyL2cR9ow";
+  // const token =
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUyMTY3N2M1NWU3ZTAwMThmODNjMmUiLCJpYXQiOjE2OTk4Nzg1MjEsImV4cCI6MTcwMTA4ODEyMX0.OUjIWiA8Coq8XIN7HBfmcox3tAoSACAyfSlyL2cR9ow";
 
   return (
     <StyledSide className={"mx-1"}>
